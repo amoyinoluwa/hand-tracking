7b3c377a8fcb9be69c8ca62105796e823ae80556
amoyinoluwa <amoyinoluwa24@gmail.com>
2024-02-10 22:19:25 -0500

    fixes

--- predict.py
+++ predict.py
@@ -13,7 +13,7 @@ class HandMovementPredictor:
 
     def create_model(self):
         model = Sequential()
-        model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(X.shape[1], X.shape[2], 1)))
+        model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(self.X.shape[1], self.X.shape[2], 1)))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Flatten())
         model.add(Dense(64, activation='relu'))
